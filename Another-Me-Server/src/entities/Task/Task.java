a42dcecdb739cde4cf7e2bca57ffe4ee52b2255a
eldariko <bizybone15@gmail.com>
2015-09-21 23:38:27 +0300

    k

---
+++ Another-Me-Server/src/entities/Task/Task.java
@@ -0,0 +1,14 @@
+package entities.Task;
+
+import java.util.ArrayList;
+
+public class Task {
+
+ String sms;
+ ArrayList<Task> completedTasks;
+ ArrayList<Task> toDoTasks;
+ ArrayList<Task> aplicationTasks;
+ 
+
+	
+}
---
+++ Another-Me-Server/src/entities/person/Person.java
@@ -0,0 +1,12 @@
+package entities.person;
+
+public class Person {
+private String name;
+String mail;
+int age;
+//class Task;
+//class Gps;
+//class Survey ?;
+	
+	
+}
