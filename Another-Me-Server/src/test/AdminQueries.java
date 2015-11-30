d6184566cb07c5f3f45ddc7ec27e861ff0270a05
eldariko <bizybone15@gmail.com>
2015-11-30 21:47:17 +0200

    X

--- Another-Me-Server/src/test/AdminQueries.java
+++ Another-Me-Server/src/test/AdminQueries.java
@@ -1,8 +1,11 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import entities.GPS.Gps;
+import entities.Task.PopUp;
+import entities.Task.PopUpTemplates;
 import entities.Task.Task;
 import entities.Task.TasksBank;
 import entities.person.Person;
@@ -12,8 +15,10 @@ public interface AdminQueries {
 	public void addUser(String name,int id,String mail,int age);
 	public Person getUser(int id);
 	public ArrayList<SMS> getAllSms(int userId);
-	public boolean addNewTask(,,,);
-public void setTasksFromCalendar(List<TaskJSON> listOfTasks);
+	public boolean addNewTask();
+public 	List<PopUp> getPopUps(); 
+//public List<PopUpTemplates>
+//	public void setTasksFromCalendar(List<TaskJSON> listOfTasks);
 public ArrayList<Task> getTasks(int userId);
 public void setGpsLocation(int userId,int x,int y);
 public Gps getGpsLocation(int userId);
